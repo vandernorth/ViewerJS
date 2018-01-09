5be287bb325411d4be81d4eb0b42c48d5357eef4
Paul Schupp <paul.schupp@hotmail.fr>
2018-01-09 18:55:54 +0100

    no more blur, and pdf.js updated

--- test/server.js
+++ test/server.js
@@ -2,18 +2,19 @@
 
 const express = require('express'),
       app     = express();
-
+      port = 8181
 app.use('/', express.static('./release'));
 app.use('/files', express.static('./test/files'));
-app.listen(81);
+app.listen(port);
 
-console.log(`Listening on port 81.
+
+console.log(`Listening on port ${port}.
  Try one of the following tests:
-  - http://localhost:81/index.html#/files/pdf-test.pdf
-  - http://localhost:81/index.html#/files/small.mp4
-  - http://localhost:81/index.html#/files/small.webm
-  - http://localhost:81/index.html#/files/test.png
-  - http://localhost:81/index.html#/files/text.txt
-  - http://localhost:81/index.html#/files/unknown.zfx
+  - http://localhost:${port}/index.html#/files/pdf-test.pdf
+  - http://localhost:${port}/index.html#/files/small.mp4
+  - http://localhost:${port}/index.html#/files/small.webm
+  - http://localhost:${port}/index.html#/files/test.png
+  - http://localhost:${port}/index.html#/files/text.txt
+  - http://localhost:${port}/index.html#/files/unknown.zfx
   
   `);
